ec37ba8616e3814b3c9b9a3e00654a70fa8c356d
Runbo Zhao <rzhao03@syr.edu>
2018-12-17 16:46:34 -0500

    DP problem.

--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-547.[Friend Circles]()
+# Dynamic Programming
+
+In a simple language, DP is to use more space to make your algorithm more efficiency. In other words, if there are many repetition in the algorithm, we can store the intermediate results in order to use it afterward.
+简而言之，以空间换取时间。
+
+## Characteristics of DP Problem
+
+1. Try to find the minimal or maximal solution of the problem;
+2. Try to find the number of the available solutions of the problem;
+3. Try to judge whether the solution is workable.
+
+## Typical DP Problem
+
+[_this part is referenced from the Hongkai's Blog_](http://kaicoding.blogspot.com/2016/07/dynamic-programming-summary-leetcode.html)
--- src/com/leetcode/exercise/Solution547.java
+++ src/com/leetcode/exercise/Solution547.java
@@ -6,4 +6,31 @@ package com.leetcode.exercise;
  * @since: 9/29/2018 3:55 PM
  */
 public class Solution547 {
+    private boolean[] visited;
+
+    public int findCircleNum(int[][] M) {
+        if(M == null || M.length == 0 || M[0].length == 0) return 0;
+
+        int numOfPeople = M.length;
+        visited = new boolean[numOfPeople];
+
+        int numOfGroup = 0;
+
+        for(int i = 0; i < numOfPeople; i++) {
+            if(visited[i] == false) {
+                numOfGroup++;
+                dfs(i, M);
+            }
+        }
+        return numOfGroup;
+    }
+
+    private void dfs(int person, int[][] M) {
+        visited[person] = true;
+        for(int friend = 0; friend < M[person].length; friend++) {
+            if(M[person][friend] == 1 && visited[friend] == false) {
+                dfs(friend, M);
+            }
+        }
+    }
 }
